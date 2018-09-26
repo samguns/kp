0995d0c56fa9a98fa897f4ef0f02057388ddf1da
samguns <wang.vai@gmail.com>
2018-09-26 15:51:33 +0800

    Oh no

--- IK_debug.py
+++ IK_debug.py
@@ -26,17 +26,35 @@ test_cases = {1:[[[2.16135,-1.42635,1.55109],
               5:[]}
 
 q1, q2, q3, q4, q5, q7, q7 = symbols('q1:8')
-d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+# d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+# a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+# alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 # DH Parameters
-dh = {alpha0:    0,      a0:     0,      d1: 0.75,
-     alpha1:    -pi/2,  a1:     0.35,   d2: 0,      q2: q2-pi/2,
-     alpha2:    0,      a2:     1.25,   d3: 0,
-     alpha3:    -pi/2,  a3:     -0.054, d4: 1.50,
-     alpha4:    pi/2,   a4:     0,      d5: 0,
-     alpha5:    -pi/2,  a5:     0,      d6: 0,
-     alpha6:    0,      a6:     0,      d7: 0.303,  q7: 0}
+# dh = {alpha0:    0,      a0:     0,      d1: 0.75,
+#      alpha1:    -pi/2,  a1:     0.35,   d2: 0,      q2: q2-pi/2,
+#      alpha2:    0,      a2:     1.25,   d3: 0,
+#      alpha3:    -pi/2,  a3:     -0.054, d4: 1.50,
+#      alpha4:    pi/2,   a4:     0,      d5: 0,
+#      alpha5:    -pi/2,  a5:     0,      d6: 0,
+#      alpha6:    0,      a6:     0,      d7: 0.303,  q7: 0}
+a0 = 0
+alpha0 = 0
+d1 = 0.75
+alpha1 = -pi/2
+a1 = 0.35
+alpha2 = 0
+a2 = 1.25
+d2 = 0
+alpha3 = -pi/2
+a3 = -0.054
+d3 = 0
+d4 = 1.5
+a4 = 0
+alpha4 = pi/2
+alpha5 = -pi/2
+d5 = 0
+alpha6 = 0
+d7 = 0.303
 
 def test_code(test_case):
     ## Set up code
@@ -81,7 +99,22 @@ def test_code(test_case):
     zc = wc[2]
     
     theta1 = atan2(yc, xc)
-    theta3 = 0
+    # Revert xc back to x-z plane
+    x = xc / cos(theta1)
+    # Translate x-z plane, such that its origin is joint2
+    x = x - a1
+    z = zc - d1
+    offset = atan2(a3, d4)
+    # Because:
+    #   x = a2 * sin(theta2) + d4 * cos(theta2 + theta3 - offset)
+    #   z = a2 * cos(theta2) - d4 * sin(theta2 + theta3 - offset)
+    #   x^2 + z^2 = a2^2 + d4^2 - 2 * a2 * d4 * sin(theta3 - offset)
+    # So:
+    #   (theta3 - offset) is known
+    s3_off = (-x**2 - z**2 + a2**2 + d4**2) / (2 * a2 * d4)
+    c3_off = sqrt(1 - s3_off**2)
+    theta3_off = atan2(s3_off, c3_off)
+    theta3 = theta3_off + offset
     theta4 = 0
     theta5 = 0
     theta6 = 0
@@ -109,6 +142,10 @@ def test_code(test_case):
     T3_4 = Matrix([[cos(q4), -sin(q4), 0, a3],
                    [sin(q4) * cos(alpha3), cos(q4) * cos(alpha3), -sin(alpha3), -sin(alpha3) * d4],
                    [sin(q4) * sin(alpha3), cos(q4) * sin(alpha3), cos(alpha3), cos(alpha3) * d4],
+                   [0, 0, 0, 1]])
+    T4_5 = Matrix([[cos(q5), -sin(q5), 0, a4],
+                   [sin(q5) * cos(alpha4), cos(q5) * cos(alpha4), -sin(alpha4), -sin(alpha4) * d5],
+                   [sin(q5) * sin(alpha4), cos(q5) * sin(alpha4), cos(alpha4), cos(alpha4) * d5],
                    [0, 0, 0, 1]])
     T0_4 = T0_1 * T1_2 * T2_3 * T3_4
 
