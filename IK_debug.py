b5acc5f1887294bcbf7c38cc36d75aea44875182
samguns <wang.vai@gmail.com>
2018-09-29 00:17:18 +0800

    Continue

--- IK_debug.py
+++ IK_debug.py
@@ -26,7 +26,7 @@ test_cases = {1:[[[2.16135,-1.42635,1.55109],
               4:[],
               5:[]}
 
-q1, q2, q3, q4, q5, q7, q7 = symbols('q1:8')
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
 # d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
 # a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
 # alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
@@ -54,6 +54,7 @@ a4 = 0
 alpha4 = pi/2
 alpha5 = -pi/2
 d5 = 0
+d6 = 0
 alpha6 = 0
 d7 = 0.303
 t = sqrt(a3 ** 2 + d4 ** 2)
@@ -109,7 +110,7 @@ def test_code(test_case):
     yc = wc[1]
     zc = wc[2]
 
-    theta1 = atan2(yc, xc)
+    dir = atan2(yc, xc).evalf()
     x = sqrt(xc ** 2 + yc ** 2)
     # Translate x-z plane, such that its origin is joint2
     # Because joint2 and joint3 are both revolute, the wrist center
@@ -120,27 +121,66 @@ def test_code(test_case):
 
     z = zc - d1
 
+    # To be sure a solution exists, according to law of cosine,
+    # sqrt(x^2 + z^2) must be <= a2 + t
+    solvable_1 = False
+    solvable_2 = False
+    if sqrt(x_1**2 + z**2) <= (a2 + t):
+        solvable_1 = True
+
+    if sqrt(x_2**2 + z**2) <= (a2 + t):
+        solvable_2 = True
+
     # Because:
     #   x = a2 * sin(theta2) + t * cos(theta2 + (theta3 + offset))
     #   z = a2 * cos(theta2) - t * sin(theta2 + (theta3 + offset))
     #   x^2 + z^2 = a2^2 + t^2 - 2 * a2 * t * sin(theta3 + offset)
-    s3_off_1 = (a2**2 + t**2 - x_1**2 - z**2) / (2 * a2 * t)
-    s3_off_2 = (a2**2 + t**2 - x_2**2 - z**2) / (2 * a2 * t)
-    c3_off_1 = sqrt(1 - s3_off_1**2)
-    c3_off_2 = sqrt(1 - s3_off_2**2)
-    theta3_1 = atan2(s3_off_1, c3_off_1) - offset
-    theta3_2 = -atan2(s3_off_2, c3_off_2) - offset - math.pi
-
     #   k1 = a2 - t * s3_off
     #   k2 = t * c3_off
     #   delta + theta2 = atan2(x, z) and delta = atan2(k2, k1)
     #   theta2 = atan2(x, z) - atan2(k2, k1)
-    k1_1 = a2 - t * s3_off_1
-    k1_2 = a2 - t * s3_off_2
-    k2_1 = t * c3_off_1
-    k2_2 = t * c3_off_2
-    theta2_1 = atan2(x_1, z) - atan2(k2_1, k1_1)
-    theta2_2 = -(atan2(x_2, z) - atan2(k2_2, k1_2))
+    theta1_1 = None
+    theta2_1 = None
+    theta3_1 = None
+    total_1 = 0
+    if solvable_1 is True:
+        s3_off_1 = (a2**2 + t**2 - x_1**2 - z**2) / (2 * a2 * t)
+        c3_off_1 = sqrt(1 - s3_off_1 ** 2)
+        theta3_1 = atan2(s3_off_1, c3_off_1) - offset
+        k1_1 = a2 - t * s3_off_1
+        k2_1 = t * c3_off_1
+        theta2_1 = atan2(x_1, z) - atan2(k2_1, k1_1)
+        theta1_1 = dir
+        total_1 = abs(theta1_1.evalf()) + abs(theta2_1.evalf()) + abs(theta3_1.evalf())
+
+    theta1_2 = None
+    theta2_2 = None
+    theta3_2 = None
+    total_2 = 0
+    if solvable_2 is True:
+        s3_off_2 = (a2**2 + t**2 - x_2**2 - z**2) / (2 * a2 * t)
+        c3_off_2 = sqrt(1 - s3_off_2**2)
+        theta3_2 = -atan2(s3_off_2, c3_off_2) - offset - math.pi
+        k1_2 = a2 - t * s3_off_2
+        k2_2 = t * c3_off_2
+        theta2_2 = -(atan2(x_2, z) - atan2(k2_2, k1_2))
+        if dir < 0:
+            theta1_2 = dir + math.pi
+        else:
+            theta1_2 = dir - math.pi
+        total_2 = abs(theta1_2.evalf()) + abs(theta2_2.evalf()) + abs(theta3_2.evalf())
+
+    theta1 = 0
+    theta2 = 0
+    theta3 = 0
+    if total_1 <= total_2:
+        theta1 = theta1_1
+        theta2 = theta2_1
+        theta3 = theta3_1
+    else:
+        theta1 = theta1_2
+        theta2 = theta2_2
+        theta3 = theta3_2
 
     theta4 = 0
     theta5 = 0
@@ -148,12 +188,7 @@ def test_code(test_case):
 
     ## 
     ########################################################################################
-    
-    ########################################################################################
-    ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
-    ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
-    ## (OPTIONAL) YOUR CODE HERE!
     T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
                    [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
                    [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
@@ -174,13 +209,84 @@ def test_code(test_case):
                    [sin(q5) * cos(alpha4), cos(q5) * cos(alpha4), -sin(alpha4), -sin(alpha4) * d5],
                    [sin(q5) * sin(alpha4), cos(q5) * sin(alpha4), cos(alpha4), cos(alpha4) * d5],
                    [0, 0, 0, 1]])
-    T0_4 = T0_1 * T1_2 * T2_3 * T3_4
+    T5_6 = Matrix([[cos(q6), -sin(q6), 0, a5],
+                   [sin(q6) * cos(alpha5), cos(q6) * cos(alpha5), -sin(alpha5), -sin(alpha5) * d6],
+                   [sin(q6) * sin(alpha5), cos(q6) * sin(alpha5), cos(alpha5), cos(alpha5) * d6],
+                   [0, 0, 0, 1]])
+    T6_G = Matrix([[cos(q7), -sin(q6), 0, a6],
+                   [sin(q7) * cos(alpha6), cos(q7) * cos(alpha6), -sin(alpha6), -sin(alpha6) * d7],
+                   [sin(q7) * sin(alpha6), cos(q7) * sin(alpha6), cos(alpha6), cos(alpha6) * d7],
+                   [0, 0, 0, 1]])
+    T0_3 = T0_1 * T1_2 * T2_3
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(req.poses.orientation.x,
+                                                                  req.poses.orientation.y,
+                                                                  req.poses.orientation.z,
+                                                                  req.poses.orientation.w)
+
+    Rc_y = Matrix([[cos(-math.pi/2),     0,      sin(-math.pi/2),    0],
+                  [0,                   1,      0,                  0],
+                  [-sin(-math.pi/2),    0,      cos(-math.pi/2),    0],
+                  [0,                   0,      0,                  1]])
+    Rc_z = Matrix([[cos(math.pi),        -sin(math.pi),  0,  0],
+                  [sin(math.pi),        cos(math.pi),   0,  0],
+                  [0,                   0,              1,  0],
+                  [0,                   0,              0,  1]])
+    R_corr = Rc_z * Rc_y
+
+    def rot_x(q):
+        R_x = Matrix([[1, 0, 0],
+                      [0, cos(q), -sin(q)],
+                      [0, sin(q), cos(q)]])
+        return R_x
+
+    def rot_y(q):
+        R_y = Matrix([[cos(q), 0, sin(q)],
+                      [0, 1, 0],
+                      [-sin(q), 0, cos(q)]])
+        return R_y
+
+    def rot_z(q):
+        R_z = Matrix([[cos(q), -sin(q), 0],
+                      [sin(q), cos(q), 0],
+                      [0, 0, 1]])
+        return R_z
+
+    Rrpy = rot_z(yaw) * rot_y(pitch) * rot_x(roll) * R_corr[:3,:3]
+    EE = Matrix[req.poses.position.x, req.poses.position.y, req.poses.position.z]
+    wrist_center = EE - (d6 + d7) * Rrpy[:,2]
+
+    R3_6 = (T0_3.inv("LU") * Rrpy).evalf(subs={q1: theta1, q2: theta2-pi/2, q3: theta3})
+    r21 = R3_6[1, 0]
+    r11 = R3_6[0, 0]
+    r31 = R3_6[2, 0]
+    r32 = R3_6[2, 1]
+    r33 = R3_6[2, 2]
+    alpha = atan2(r21, r11) # rotation about Z-axis
+    beta = atan2(-r31, sqrt(r11 * r11 + r21 * r21))  # rotation about Y-axis
+    gamma = atan2(r32, r33)  # rotation about X-axis
+
+    ########################################################################################
+    ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
+    ## as the input and output the position of your end effector as your_ee = [x,y,z]
+
+    ## (OPTIONAL) YOUR CODE HERE!
+    T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G * R_corr
+
+    T_EE = T0_G.evalf(subs={q1: theta1, q2: theta2-pi/2,  q3: theta3,
+                            q4: theta4, q5: theta5, q6: theta6, q7: 0})
+
+    # if solvable_1 == True:
+    #     print(T0_4.evalf(subs={q1: theta1_1, q2: theta2_1-pi/2, q3: theta3_1, q4: 0}))
+    #
+    # if solvable_2 == True:
+    #     print(T0_4.evalf(subs={q1: theta1_2, q2: theta2_2-pi/2, q3: theta3_2, q4: 0}))
 
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
+    your_wc = wrist_center # <--- Load your calculated WC values in this array
     your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
@@ -232,6 +338,6 @@ def test_code(test_case):
 
 if __name__ == "__main__":
     # Change test case number for different scenarios
-    test_case_number = 1
+    test_case_number = 3
 
     test_code(test_cases[test_case_number])
